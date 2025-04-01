05c1c782712aca1e311988a595f379779410be4e
Barehands <121155435+barehands-io@users.noreply.github.com>
2025-04-01 20:18:24 +0100

    initialize home component and meta function

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -9,5 +9,16 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return  <div>
+    <h1>React Router</h1>
+    <p>
+      This is a simple example of a React Router application. It demonstrates
+      the use of nested routes and layout routes.
+    </p>
+    <p>
+      To learn more about React Router, check out the{" "}
+  
+      .
+    </p>
+  </div>
 }
